55d5aa264cd4ed39e2cffb366e2459154c42f8f0
10234567Z <93607971+10234567Z@users.noreply.github.com>
2024-05-07 10:36:53 +0530

    Remake the services-grid layout

--- src/app/components/services.tsx
+++ src/app/components/services.tsx
@@ -12,34 +12,95 @@ export default function Services() {
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum ullam odit iusto deleniti quam, error, dignissimos corr aperiam reprehenderit praesentium corporis eveniet voluptatum non est harum, aliquid corrupti et tenetur sapiente.
             </p>
             <div className="grid lg:grid-cols-auto-fit-300 gap-2 mt-4 grid-cols-auto-fit-200  w-[90%] sm:w-[624px] lg:w-[924px] place-items-center">
-                <div className="max-w-[300px] max-h-[300px]  overflow-hidden">
-                    <Image src='/stocki2.jpg' alt="img1" width={300} height={300} className="h-[300px] transition-all duration-500 ease-linear hover:scale-125 hidden lg:block " />
-                    <Image src='/stocki2.jpg' alt="img1" width={200} height={200} className="h-[200px] transition-all duration-500 ease-linear hover:scale-125 block lg:hidden " />
+                <div className="max-w-[300px] max-h-[300px] flex flex-col content-start text-left justify-center gap-2 bg-slate-300 rounded-lg overflow-hidden ease-linear duration-[350ms] transition-all hover:scale-105 ">
+                    <div className="max-w-[300px] h-[150px]  overflow-hidden">
+                        <Image src='/stocki1.jpg' alt="img1" width={300} height={150} className="w-full h-[150px] hidden lg:block " />
+                        <Image src='/stocki1.jpg' alt="img1" width={200} height={150} className="w-full h-[150px] block lg:hidden " />
+                    </div>
+                    <div className="p-4 flex flex-col">
+                        <h4 className="text-xl font-bold">Title</h4>
+                        <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. s eveniet voluptatum non est harum, aliquid corrupti et tenetur sapiente.</p>
+                    </div>
                 </div>
-                <div className="max-w-[300px] max-h-[300px]  overflow-hidden">
-                    <Image src='/stocki3.jpg' alt="img1" width={300} height={300} className="h-[300px] transition-all duration-500 ease-linear hover:scale-125 hidden lg:block " />
-                    <Image src='/stocki3.jpg' alt="img1" width={200} height={200} className="h-[200px] transition-all duration-500 ease-linear hover:scale-125 block lg:hidden " />                </div>
-                <div className="max-w-[300px] max-h-[300px]  overflow-hidden">
-                    <Image src='/stocki4.jpg' alt="img1" width={300} height={300} className="h-[300px] transition-all duration-500 ease-linear hover:scale-125 hidden lg:block " />
-                    <Image src='/stocki4.jpg' alt="img1" width={200} height={200} className="h-[200px] transition-all duration-500 ease-linear hover:scale-125 block lg:hidden " />                </div>
-                <div className="max-w-[300px] max-h-[300px]  overflow-hidden">
-                    <Image src='/stocki1.jpg' alt="img1" width={300} height={300} className="h-[300px] transition-all duration-500 ease-linear hover:scale-125 hidden lg:block " />
-                    <Image src='/stocki1.jpg' alt="img1" width={200} height={200} className="h-[200px] transition-all duration-500 ease-linear hover:scale-125 block lg:hidden " />                </div>
-                <div className="max-w-[300px] max-h-[300px]  overflow-hidden">
-                    <Image src='/stocki2.jpg' alt="img1" width={300} height={300} className="h-[300px] transition-all duration-500 ease-linear hover:scale-125 hidden lg:block " />
-                    <Image src='/stocki2.jpg' alt="img1" width={200} height={200} className="h-[200px] transition-all duration-500 ease-linear hover:scale-125 block lg:hidden " />                </div>
-                <div className="max-w-[300px] max-h-[300px]  overflow-hidden">
-                    <Image src='/stocki3.jpg' alt="img1" width={300} height={300} className="h-[300px] transition-all duration-500 ease-linear hover:scale-125 hidden lg:block " />
-                    <Image src='/stocki3.jpg' alt="img1" width={200} height={200} className="h-[200px] transition-all duration-500 ease-linear hover:scale-125 block lg:hidden " />                </div>
-                <div className="max-w-[300px] max-h-[300px]  overflow-hidden">
-                    <Image src='/stocki4.jpg' alt="img1" width={300} height={300} className="h-[300px] transition-all duration-500 ease-linear hover:scale-125 hidden lg:block " />
-                    <Image src='/stocki4.jpg' alt="img1" width={200} height={200} className="h-[200px] transition-all duration-500 ease-linear hover:scale-125 block lg:hidden " />                </div>
-                <div className="max-w-[300px] max-h-[300px]  overflow-hidden">
-                    <Image src='/stocki1.jpg' alt="img1" width={300} height={300} className="h-[300px] transition-all duration-500 ease-linear hover:scale-125 hidden lg:block " />
-                    <Image src='/stocki1.jpg' alt="img1" width={200} height={200} className="h-[200px] transition-all duration-500 ease-linear hover:scale-125 block lg:hidden " />                </div>
-                <div className="max-w-[300px] max-h-[300px]  overflow-hidden">
-                    <Image src='/stocki3.jpg' alt="img1" width={300} height={300} className="h-[300px] transition-all duration-500 ease-linear hover:scale-125 hidden lg:block " />
-                    <Image src='/stocki3.jpg' alt="img1" width={200} height={200} className="h-[200px] transition-all duration-500 ease-linear hover:scale-125 block lg:hidden " />
+                <div className="max-w-[300px] max-h-[300px] flex flex-col content-start text-left justify-center gap-2 bg-slate-300 rounded-lg overflow-hidden ease-linear duration-[350ms] transition-all hover:scale-105 ">
+                    <div className="max-w-[300px] h-[150px]  overflow-hidden">
+                        <Image src='/stocki2.jpg' alt="img2" width={300} height={150} className="w-full h-[150px] hidden lg:block " />
+                        <Image src='/stocki2.jpg' alt="img2" width={200} height={150} className="w-full h-[150px] block lg:hidden " />
+                    </div>
+                    <div className="p-4 flex flex-col">
+                        <h4 className="text-xl font-bold">Title</h4>
+                        <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. s eveniet voluptatum non est harum, aliquid corrupti et tenetur sapiente.</p>
+                    </div>
+                </div>
+                <div className="max-w-[300px] max-h-[300px] flex flex-col content-start text-left justify-center gap-2 bg-slate-300 rounded-lg overflow-hidden ease-linear duration-[350ms] transition-all hover:scale-105 ">
+                    <div className="max-w-[300px] h-[150px]  overflow-hidden">
+                        <Image src='/stocki3.jpg' alt="img3" width={300} height={150} className="w-full h-[150px] hidden lg:block " />
+                        <Image src='/stocki3.jpg' alt="img3" width={200} height={150} className="w-full h-[150px] block lg:hidden " />
+                    </div>
+                    <div className="p-4 flex flex-col">
+                        <h4 className="text-xl font-bold">Title</h4>
+                        <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. s eveniet voluptatum non est harum, aliquid corrupti et tenetur sapiente.</p>
+                    </div>
+                </div>
+                <div className="max-w-[300px] max-h-[300px] flex flex-col content-start text-left justify-center gap-2 bg-slate-300 rounded-lg overflow-hidden ease-linear duration-[350ms] transition-all hover:scale-105 ">
+                    <div className="max-w-[300px] h-[150px]  overflow-hidden">
+                        <Image src='/stocki4.jpg' alt="img4" width={300} height={150} className="w-full h-[150px] hidden lg:block " />
+                        <Image src='/stocki4.jpg' alt="img4" width={200} height={150} className="w-full h-[150px] block lg:hidden " />
+                    </div>
+                    <div className="p-4 flex flex-col">
+                        <h4 className="text-xl font-bold">Title</h4>
+                        <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. s eveniet voluptatum non est harum, aliquid corrupti et tenetur sapiente.</p>
+                    </div>
+                </div>
+                <div className="max-w-[300px] max-h-[300px] flex flex-col content-start text-left justify-center gap-2 bg-slate-300 rounded-lg overflow-hidden ease-linear duration-[350ms] transition-all hover:scale-105 ">
+                    <div className="max-w-[300px] h-[150px]  overflow-hidden">
+                        <Image src='/stocki1.jpg' alt="img1" width={300} height={150} className="w-full h-[150px] hidden lg:block " />
+                        <Image src='/stocki1.jpg' alt="img1" width={200} height={150} className="w-full h-[150px] block lg:hidden " />
+                    </div>
+                    <div className="p-4 flex flex-col">
+                        <h4 className="text-xl font-bold">Title</h4>
+                        <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. s eveniet voluptatum non est harum, aliquid corrupti et tenetur sapiente.</p>
+                    </div>
+                </div>
+                <div className="max-w-[300px] max-h-[300px] flex flex-col content-start text-left justify-center gap-2 bg-slate-300 rounded-lg overflow-hidden ease-linear duration-[350ms] transition-all hover:scale-105 ">
+                    <div className="max-w-[300px] h-[150px]  overflow-hidden">
+                        <Image src='/stocki2.jpg' alt="img2" width={300} height={150} className="w-full h-[150px] hidden lg:block " />
+                        <Image src='/stocki2.jpg' alt="img2" width={200} height={150} className="w-full h-[150px] block lg:hidden " />
+                    </div>
+                    <div className="p-4 flex flex-col">
+                        <h4 className="text-xl font-bold">Title</h4>
+                        <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. s eveniet voluptatum non est harum, aliquid corrupti et tenetur sapiente.</p>
+                    </div>
+                </div>
+                <div className="max-w-[300px] max-h-[300px] flex flex-col content-start text-left justify-center gap-2 bg-slate-300 rounded-lg overflow-hidden ease-linear duration-[350ms] transition-all hover:scale-105 ">
+                    <div className="max-w-[300px] h-[150px]  overflow-hidden">
+                        <Image src='/stocki3.jpg' alt="img3" width={300} height={150} className="w-full h-[150px] hidden lg:block " />
+                        <Image src='/stocki3.jpg' alt="img3" width={200} height={150} className="w-full h-[150px] block lg:hidden " />
+                    </div>
+                    <div className="p-4 flex flex-col">
+                        <h4 className="text-xl font-bold">Title</h4>
+                        <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. s eveniet voluptatum non est harum, aliquid corrupti et tenetur sapiente.</p>
+                    </div>
+                </div>
+                <div className="max-w-[300px] max-h-[300px] flex flex-col content-start text-left justify-center gap-2 bg-slate-300 rounded-lg overflow-hidden ease-linear duration-[350ms] transition-all hover:scale-105 ">
+                    <div className="max-w-[300px] h-[150px]  overflow-hidden">
+                        <Image src='/stocki4.jpg' alt="img4" width={300} height={150} className="w-full h-[150px] hidden lg:block " />
+                        <Image src='/stocki4.jpg' alt="img4" width={200} height={150} className="w-full h-[150px] block lg:hidden " />
+                    </div>
+                    <div className="p-4 flex flex-col">
+                        <h4 className="text-xl font-bold">Title</h4>
+                        <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. s eveniet voluptatum non est harum, aliquid corrupti et tenetur sapiente.</p>
+                    </div>
+                </div>
+                <div className="max-w-[300px] max-h-[300px] flex flex-col content-start text-left justify-center gap-2 bg-slate-300 rounded-lg overflow-hidden ease-linear duration-[350ms] transition-all hover:scale-105 ">
+                    <div className="max-w-[300px] h-[150px]  overflow-hidden">
+                        <Image src='/stocki1.jpg' alt="img1" width={300} height={150} className="w-full h-[150px] hidden lg:block " />
+                        <Image src='/stocki1.jpg' alt="img1" width={200} height={150} className="w-full h-[150px] block lg:hidden " />
+                    </div>
+                    <div className="p-4 flex flex-col">
+                        <h4 className="text-xl font-bold">Title</h4>
+                        <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. s eveniet voluptatum non est harum, aliquid corrupti et tenetur sapiente.</p>
+                    </div>
                 </div>
             </div>
             <hr className="w-[45%] border-1 border-blue-900 my-4" />
