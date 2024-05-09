5b9902caa78f9b5ee9dddf4a3b4d88da6260b282
10234567Z <93607971+10234567Z@users.noreply.github.com>
2024-05-09 17:56:01 +0530

    Replace placeholder text in about-us page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ import Missions from "../components/missions";
 
 export default function Home() {
     return (
-        <main className=" flex flex-col items-start justify-center gap-8 mt-4 sm:mt-16 p-4 pb-0">
+        <main className=" flex flex-col items-center justify-center gap-8 mt-4 sm:mt-16 p-4 pb-0">
             <Image
                 src={"/logo.png"}
                 width={100}
@@ -12,24 +12,9 @@ export default function Home() {
                 alt="Logo"
                 className="block sm:hidden self-center"
             />
-            <h1 className="text-4xl text-blue-900 font-bold text-left">
-                Managing Property is hard
+            <h1 className="text-4xl text-blue-900 font-bold">
+                Managing Property is hard - We make it easy
             </h1>
-            <h1 className="text-3xl text-blue-900 font-bold text-left">
-                We make it easy
-            </h1>
-            <p className="text-lg max-w-[80%] text-left leading-8">
-                Our venture into property management stemmed from a personal quest for
-                excellence. Faced with the challenges of finding a reliable management
-                company for our own assets, we recognized a glaring gap in the industry.
-                Our commitment is built upon rectifying these deficiencies; we
-                prioritize responsiveness, fairness, and a thorough understanding of the
-                investment landscape. By leveraging our firsthand experience as property
-                owners, we&apos;ve crafted a service that prioritizes your needs and ensures
-                fiscal responsibility. Entrust us with your property, and experience the
-                ease and peace of mind that comes with a dedicated partner invested in
-                your success.
-            </p>
             <div className="flex flex-col items-start justify-center gap-4 mt-4 p-16 bg-slate-300 max-w-[620px] lg:max-w-[1000px] rounded-xl">
                 <h1 className="text-2xl font-bold text-blue-900">
                     How we started?
--- src/app/components/team.tsx
+++ src/app/components/team.tsx
@@ -4,49 +4,49 @@ export default function Team() {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 place-items-start p-4 gap-8">
             <div className="flex flex-row justify-center content-center gap-4">
-                <Image src="/team/man.png" width={75} height={75} alt="Michael" className="rounded-full" />
+                <Image src="/team/man.png" width={75} height={75} alt="Michael" className="rounded-full w-[75px] h-[75px]" />
                 <div className="flex flex-col justify-center content-center gap-4">
                     <h1 className="text-xl font-bold text-blue-900">Michael</h1>
                     <p className="text-lg text-blue-950">VP of Development</p>
                 </div>
             </div>
             <div className="flex flex-row justify-center content-center gap-4">
-                <Image src="/team/man.png" width={75} height={75} alt="Michael" className="rounded-full" />
+                <Image src="/team/man.png" width={75} height={75} alt="Michael" className="rounded-full w-[75px] h-[75px]" />
                 <div className="flex flex-col justify-center content-center gap-4">
                     <h1 className="text-xl font-bold text-blue-900">Alex</h1>
                     <p className="text-lg text-blue-950">VP of Operations</p>
                 </div>
             </div>
             <div className="flex flex-row justify-center content-center gap-4">
-                <Image src="/team/man.png" width={75} height={75} alt="Michael" className="rounded-full" />
+                <Image src="/team/man.png" width={75} height={75} alt="Michael" className="rounded-full w-[75px] h-[75px]" />
                 <div className="flex flex-col justify-center content-center gap-4">
                     <h1 className="text-xl font-bold text-blue-900">Julian</h1>
                     <p className="text-lg text-blue-950">Director</p>
                 </div>
             </div>
             <div className="flex flex-row justify-center content-center gap-4">
-                <Image src="/team/man.png" width={75} height={75} alt="Michael" className="rounded-full" />
+                <Image src="/team/man.png" width={75} height={75} alt="Michael" className="rounded-full w-[75px] h-[75px]" />
                 <div className="flex flex-col justify-center content-center gap-4">
                     <h1 className="text-xl font-bold text-blue-900">Fred</h1>
                     <p className="text-lg text-blue-950">VP of Finance</p>
                 </div>
             </div>
             <div className="flex flex-row justify-center content-center gap-4">
-                <Image src="/team/man.png" width={75} height={75} alt="Michael" className="rounded-full" />
+                <Image src="/team/man.png" width={75} height={75} alt="Michael" className="rounded-full w-[75px] h-[75px]" />
                 <div className="flex flex-col justify-center content-center gap-4">
                     <h1 className="text-xl font-bold text-blue-900">Laurie</h1>
                     <p className="text-lg text-blue-950">Vice President</p>
                 </div>
             </div>
             <div className="flex flex-row justify-center content-center gap-4">
-                <Image src="/team/man.png" width={75} height={75} alt="Michael" className="rounded-full" />
+                <Image src="/team/man.png" width={75} height={75} alt="Michael" className="rounded-full w-[75px] h-[75px]" />
                 <div className="flex flex-col justify-center content-center gap-4">
                     <h1 className="text-xl font-bold text-blue-900">Karen</h1>
                     <p className="text-lg text-blue-950">Director</p>
                 </div>
             </div>
             <div className="flex flex-row justify-center content-center gap-4">
-                <Image src="/team/man.png" width={75} height={75} alt="Michael" className="rounded-full" />
+                <Image src="/team/man.png" width={75} height={75} alt="Michael" className="rounded-full w-[75px] h-[75px]" />
                 <div className="flex flex-col justify-center content-center gap-4">
                     <h1 className="text-xl font-bold text-blue-900">Zoey</h1>
                     <p className="text-lg text-blue-950">Administrative Assistant</p>
