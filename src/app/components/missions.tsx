1c41080f53468d02f6df76de682f8cf27c77c94d
1023457Z <93607971+10234567Z@users.noreply.github.com>
2024-05-14 00:42:44 +0530

    Replace placeholder testimonials

--- src/app/components/missions.tsx
+++ src/app/components/missions.tsx
@@ -25,7 +25,7 @@ export default function Missions() {
                     </p>
                 </div>
                 <div className="flex flex-col justify-start content-center gap-4 w-[300px] p-8 border-2 border-solid border-black rounded-md">
-                    <h1 className="text-xl font-bold text-blue-900">Transparent</h1>
+                    <h1 className="text-xl font-bold text-blue-900">Transparency</h1>
                     <p className="text-lg text-blue-950">
                         Ensuring transparency in every aspect of property management,
                         fostering trust and accountability among our clients and residents
--- src/app/components/testimonial.tsx
+++ src/app/components/testimonial.tsx
@@ -14,7 +14,7 @@ export default function Testimonial() {
         spaceBetween={0}
         slidesPerView={1}
         speed={1200}
-        pagination={{ clickable: true, dynamicBullets: true}}
+        pagination={{ clickable: true, dynamicBullets: true }}
         modules={[Pagination, Autoplay, Navigation]}
         navigation
         autoplay={{ delay: 2000 }}
@@ -22,44 +22,40 @@ export default function Testimonial() {
       >
         <SwiperSlide>
           <div className="m-0 mx-auto">
-              <div className="flex flex-col items-center justify-center gap-4 p-12">
-                <h1 className="text-xl font-bold">Some Name</h1>
-                <p className="text-sm leading-6">
-                  &quot;a tenetur! Aut repudiandae voluptas ipsa itaque fugiat! Culpa
-                  deleniti hic eligendi aperiam rerum officia velit perferendis
-                  ipsum impedit molestiae! Dignissimos ipsum illum nobis ipsam
-                  facilis sed animi. Ut fugiat dolorum laboriosam ex id sint
-                  quisquam quas adipisci.&quot;
-                </p>
-              </div>
+            <div className="flex flex-col items-center justify-center gap-4 p-12">
+              <h1 className="text-xl font-bold">Maria S.</h1>
+              <p className="text-sm leading-6">
+                &quot;I have entrusted TPG Management with managing my 2 family
+                investment property for the last 2 years, and it has been
+                nothing short of amazing. The only thing I do each month, is
+                decide where to put my rental checks. I don&apos;t think about
+                the house or do anything further.&quot;
+              </p>
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="m-0 mx-auto">
-              <div className="flex flex-col items-center justify-center gap-4 p-12">
-                <h1 className="text-xl font-bold">Some Name</h1>
-                <p className="text-sm leading-6">
-                  &quot;a tenetur! Aut repudiandae voluptas ipsa itaque fugiat! Culpa
-                  deleniti hic eligendi aperiam rerum officia velit perferendis
-                  ipsum impedit molestiae! Dignissimos ipsum illum nobis ipsam
-                  facilis sed animi. Ut fugiat dolorum laboriosam ex id sint
-                  quisquam quas adipisci.&quot;
-                </p>
-              </div>
+            <div className="flex flex-col items-center justify-center gap-4 p-12">
+              <h1 className="text-xl font-bold">Sam C.</h1>
+              <p className="text-sm leading-6">
+                &quot;TPG Management handles my six investment properties
+                amazingly well. They&apos;re really great at what they do!&quot;
+              </p>
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="m-0 mx-auto">
-              <div className="flex flex-col items-center justify-center gap-4 p-12">
-                <h1 className="text-xl font-bold">Some Name</h1>
-                <p className="text-sm leading-6">
-                  &quot;a tenetur! Aut repudiandae voluptas ipsa itaque fugiat! Culpa
-                  deleniti hic eligendi aperiam rerum officia velit perferendis
-                  ipsum impedit molestiae! Dignissimos ipsum illum nobis ipsam
-                  facilis sed animi. Ut fugiat dolorum laboriosam ex id sint
-                  quisquam quas adipisci.&quot;
-                </p>
-              </div>
+            <div className="flex flex-col items-center justify-center gap-4 p-12">
+              <h1 className="text-xl font-bold">Brad R.</h1>
+              <p className="text-sm leading-6">
+                &quot;TPG Management takes care of my family home, the place I
+                grew up in and didn&apos;t want to sell. They handle every aspect of
+                the property and allow me to not have to worry about it,
+                especially living 500 miles away!&quot;
+              </p>
+            </div>
           </div>
         </SwiperSlide>
       </Swiper>
