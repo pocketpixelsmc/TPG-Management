ed877f656ee283b99213875677b1d9f268ba070f
10234567Z <93607971+10234567Z@users.noreply.github.com>
2024-10-12 15:43:09 +0530

    Fix google anal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import Navbar from "./components/navbar";
 import { inter, lexendDeca } from "./components/fonts";
 import Footer from "./components/footer";
-import GoogleAnalytics from "./components/GoogleAnalytics";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "TPG Management",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <GoogleAnalytics />
+      <GoogleAnalytics gaId={`${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`} />
       <body className={`${lexendDeca.className} h-full w-screen`}>
         <div className="flex flex-row items-start justify-end gap-4 w-full">
           <a href="https://tpgcompanies.managebuilding.com/Resident/portal/login" rel="noopener" target="_blank" >
