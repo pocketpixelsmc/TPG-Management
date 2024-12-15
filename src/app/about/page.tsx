fc688237c6160c9c694b937484de61e0dba327f3
Mike <23327684+pocketpixelsmc@users.noreply.github.com>
2024-12-14 20:04:20 -0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -13,11 +13,11 @@ export default function Home() {
   return (
     <main className=" flex flex-col items-center justify-center gap-8 mt-4 sm:mt-16 p-4 pb-0">
       <Image src={"/logo.png"} width={100} height={100} alt="Logo" className="block sm:hidden self-center" />
-      <h1 className="text-3xl text-blue-900 font-bold">Managing Property is hard - We make it easy</h1>
+      <h1 className="text-3xl text-blue-900 font-bold">Managing rental property is our life</h1>
       <div className="flex flex-col items-start justify-center gap-4 mt-4 p-4 sm:p-16 bg-slate-300 max-w-[500px] sm:max-w-[620px] lg:max-w-[1000px] rounded-xl">
         <h1 className="text-2xl font-bold text-blue-900">How we started?</h1>
         <p className="text-xl text-blue-950 text-left leading-loose">
-          We launched TPG Property Management as experienced investors in both multi-family and retail properties. Using the knowledge and
+          TPG Management was started by experienced investors in both multi-family and retail properties. Using the knowledge and
           infrastructure we developed while managing our own portfolio, we recognized an opportunity to assist other investors and property owners. By
           leveraging our hands-on property ownership experience, we offer comprehensive management solutions tailored to maximize property performance
           and streamline operations for our clients.
