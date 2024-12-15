976aa7afc0b44d3dba20ab398b443961b52562f2
Mike <23327684+pocketpixelsmc@users.noreply.github.com>
2024-12-14 20:08:39 -0500

    Update page.tsx

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -13,13 +13,10 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center gap-8 mt-4 mb-8 sm:mt-16 p-4 pb-0">
       <div className="flex gap-4 flex-col items-center">
-        <h1 className="text-5xl text-blue-900 font-bold">Services & Pricing</h1>
-        <h2 className="text-2xl text-blue-900 font-bold max-w-[450px] text-center">Discover how we make property management easy and stress-free.</h2>
+        <h1 className="text-5xl text-blue-900 font-bold">We provide full-service property management.</h1>
+        <h2 className="text-2xl text-blue-900 font-bold max-w-[450px] text-center">We’re hands-on, so you don’t have to be.</h2>
         <p className="text-xl text-black max-w-[800px] text-center">
-          No matter the size or type of property you own, our goal is to handle the details, so you don&apos;t have to. At TPG Management, we tailor
-          our services to meet your specific needs, offering comprehensive solutions that cover everything from tenant placement to ongoing
-          maintenance. From the start, we are committed to maximizing the value of your investment and ensuring seamless management every step of the
-          way.
+         Our people make the difference. Our backgrounds. Our strategies. Our action plans. Our execution. We strive to deliver a top-tier level service to both residents and property owners by simplifying the complicated. Easy to understand terms, simple fee structures, honesty and transparency are all part of the core values that Augusta Rental Homes strives to deliver everyday.
         </p>
       </div>
       <hr className="hidden md:block w-[90%] border-1 border-slate-800 mx-auto my-8" />
