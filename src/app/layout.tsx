dbc2e5d34226be8b260aaf97fbdbd7784a7afe31
10234567Z <93607971+10234567Z@users.noreply.github.com>
2025-03-11 01:50:47 +0530

    Change validations and modularize

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const isValidDate = checkDate('2025-03-15');
+  const isValidDate = checkDate('2035-03-15');
 
   return (
     <>
