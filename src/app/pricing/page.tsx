890aa7581cff769d5207cf1bf5c2977efe2ccf11
Mike <23327684+pocketpixelsmc@users.noreply.github.com>
2025-04-26 13:57:23 -0400

    Update page.tsx

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -15,12 +15,7 @@ export default function Home() {
     <main className="flex flex-col items-center justify-center gap-8 mt-4 p-4 pb-0 sm:mt-16">
       <div className="flex gap-4 flex-col items-center">
         <Header>Multifamily Property Management</Header>
-        <Header variant="subHeader">Dedicated Multifamily Property Management Professionals</Header>
-        <p className="text-xl text-blue-950 max-w-[800px] text-center">
-Partner with TPG Management for seamless, full-service property management tailored to your needs. From leasing and billing to groundskeeping and maintenance, 
-our experienced team works alongside you to maximize your property’s success. We have the expertise, care, and personalized support to manage everything from 
-scattered-site portfolios to large luxury high-rises—helping multifamily properties of all sizes thrive!
-        </p>
+        <Header variant="subHeader">Dedicated Multifamily Management & Maintenance Professionals</Header>
         <div className="flex flex-col sm:flex-row gap-4">
           <Link href="/contact">
             <Button>CONTACT US</Button>
