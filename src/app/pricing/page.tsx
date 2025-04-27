9ab8eb3a72d8258638808375b24e58ee83289f4b
Mike <23327684+pocketpixelsmc@users.noreply.github.com>
2025-04-27 12:06:41 -0500

    Update page.tsx

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center gap-8 mt-4 p-4 pb-0 sm:mt-16">
       <div className="flex gap-4 flex-col items-center">
-        <Header>Full Service Property Management</Header>
-        <Header variant="subHeader">Transparent pricing and fee structure for our property management services.</Header>
+        <Header>Management You Can Trust. Pricing You Can Understand.</Header>
+        <Header variant="subHeader">Experience full-service management, responsive maintenance, and expert leasing — all at fair, predictable rates.</Header>
         <div className="flex flex-col sm:flex-row gap-4">
         </div>
       </div>
