3606ec1d42617a7b2b43f293d434468f5a10880b
10234567Z <93607971+10234567Z@users.noreply.github.com>
2025-05-04 10:40:29 +0530

    Remove spacing from pricing page

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -12,8 +12,8 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center gap-8 mt-4">
-      <div className="flex gap-4 flex-col items-center">
+    <main className="flex flex-col items-center justify-center mt-4">
+      <div className="flex flex-col items-center">
         <section className="relative p-16 flex flex-col items-center justify-center text-center w-screen"
           style={{
             backgroundImage: 'url(/pricingHeader.png)',
@@ -24,8 +24,6 @@ export default function Home() {
           <Header>Management You Can Trust. Pricing You Can Understand.</Header>
           <Header variant="subHeader">Experience full-service management, responsive maintenance, and expert leasing — all at fair, predictable rates.</Header>
         </section>
-        <div className="flex flex-col sm:flex-row gap-4">
-        </div>
       </div>
       <PricingGrid />
     </main>
