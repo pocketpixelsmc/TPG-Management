34266fd1307111cbbf39e9e5406e629a9ae45cc5
10234567Z <93607971+10234567Z@users.noreply.github.com>
2025-05-15 09:52:55 +0530

    Update pricing page

--- src/app/components/Pricing/pricingGrid.tsx
+++ src/app/components/Pricing/pricingGrid.tsx
@@ -1,17 +1,20 @@
-import feesData from "@/data/fees.json";
-import servicesData from "@/data/pricingServices.json";
-import TopLeftSection from "./PricingInfoGrid/TopLeftSection";
-import TopRightSection from "./PricingInfoGrid/TopRightSection";
-import BottomLeftSection from "./PricingInfoGrid/BottomLeftSection";
-import BottomRightSection from "./PricingInfoGrid/BottomRightSection";
+// import feesData from "@/data/fees.json";
+// import servicesData from "@/data/pricingServices.json";
+// import TopLeftSection from "./PricingInfoGrid/TopLeftSection";
+// import TopRightSection from "./PricingInfoGrid/TopRightSection";
+// import BottomLeftSection from "./PricingInfoGrid/BottomLeftSection";
+// import BottomRightSection from "./PricingInfoGrid/BottomRightSection";
 
-export default function PricingGrid() {
-  return (
-    <div className="flex flex-wrap">
-      <TopLeftSection fees={feesData.fees} />
-      <TopRightSection downloads={servicesData.services} />
-      <BottomLeftSection />
-      <BottomRightSection services={feesData.downloads} />
-    </div>
-  );
-}
+// export default function PricingGrid() {
+//   return (
+//     <div className="flex flex-wrap">
+//       <TopLeftSection fees={feesData.fees} />
+//       <TopRightSection downloads={servicesData.services} />
+//       <BottomLeftSection />
+//       <BottomRightSection services={feesData.downloads} />
+//     </div>
+//   );
+// }
+
+
+/** Deprecated */
--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,9 +1,9 @@
 import { Metadata } from "next";
 import Link from "next/link";
 import OurProcess from "../components/Pricing/ourProcess";
-import PricingGrid from "../components/Pricing/pricingGrid";
 import Header from "../components/ui/Header";
 import Button from "../components/ui/Button";
+import pricingServicesData from "@/data/pricingServices.json";
 import Testimonial from "../components/testimonial";
 
 export const metadata: Metadata = {
@@ -26,8 +26,44 @@ export default function Home() {
           <Header variant="subHeader">Experience full-service management, responsive maintenance, and expert leasing — all at fair, predictable rates.</Header>
         </section>
       </div>
-      <PricingGrid />
-      <Testimonial />
+      <section className="grid grid-cols-1 lg:grid-cols-3 m-0 w-full mx-2 lg:m-0 max-w-6xl">
+        <div className="hidden lg:block"></div>
+        <div className="bg-blue-900 text-white p-4 hidden lg:flex items-center justify-center font-extrabold text-2xl">
+          Full Service Management & Leasing
+        </div>
+        <div className="bg-blue-950 text-white p-4 hidden lg:flex items-center justify-center font-extrabold text-2xl">
+          Industry Averages
+        </div>
+        {pricingServicesData.map((service, index) => (
+          <>
+            {service.title !== "Leasing Fees" && <div className="bg-red-900 px-4 lg:bg-blue-900 text-white py-6 flex items-start text-left justify-start font-xl lg:col-span-3">
+              {service.title}
+            </div>}
+            {service.items.map((item, itemIndex) => (
+              <>
+                <div className="bg-blue-900 lg:border-t-[1px] lg:border-l-[1px] lg:border-gray-600 lg:bg-gray-100 text-white lg:text-black py-6 flex items-center justify-center lg:justify-start px-4 font-xl" key={itemIndex}>
+                  {item.name}
+                </div>
+                <div className=" lg:border-t-[1px] lg:border-l-[1px] lg:border-gray-600 bg-gray-200 text-black py-6 flex items-center justify-center font-xl text-center px-2" key={itemIndex}>
+                  <span
+                    dangerouslySetInnerHTML={{ __html: item.ourProcess }}
+                  ></span>
+                </div>
+                <div className="hidden lg:border-t-[1px] lg:border-l-[1px] lg:border-gray-600 lg:bg-gray-300 lg:text-black text-white py-6 lg:flex items-center justify-center font-xl text-center px-2" key={itemIndex}>
+                  <span
+                    dangerouslySetInnerHTML={{ __html: item.industryAverage }}
+                  ></span>
+                </div>
+              </>
+            ))}
+          </>
+        ))}
+        <Button className="my-4">
+          <Link href="/contact">
+            Get Started
+          </Link>
+        </Button>
+      </section>
     </main>
   );
 }
