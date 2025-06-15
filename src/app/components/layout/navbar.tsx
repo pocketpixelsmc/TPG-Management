390a2f6f58033c156969c1fbe387fc13155fefec
Mike <23327684+pocketpixelsmc@users.noreply.github.com>
2025-06-15 18:27:40 -0400

    Update navbar.tsx

--- src/app/components/layout/navbar.tsx
+++ src/app/components/layout/navbar.tsx
@@ -110,7 +110,7 @@ export const Navbar = () => {
           </a>
           <div className="flex gap-3">
             <a
-              href="https://tpgcompanies.managebuilding.com/Resident/portal/login"
+              href="https://tpgmgmt.appfolio.com/connect/users/sign_in"
               target="_blank"
               rel="noopener noreferrer"
               className="text-xs sm:text-sm hover:text-blue-200 transition-colors"
@@ -119,7 +119,7 @@ export const Navbar = () => {
             </a>
             <span className="text-gray-400">|</span>
             <a
-              href="https://tpgcompanies.managebuilding.com/manager"
+              href="https://tpgmgmt.appfolio.com/oportal/users/log_in"
               target="_blank"
               rel="noopener noreferrer"
               className="text-xs sm:text-sm hover:text-blue-200 transition-colors"
