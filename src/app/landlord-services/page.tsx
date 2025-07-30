bcd8255936f2ae957b9078357f131464521f3109
Mike <23327684+pocketpixelsmc@users.noreply.github.com>
2025-07-30 17:05:30 -0400

    Update page.tsx

--- src/app/landlord-services/page.tsx
+++ src/app/landlord-services/page.tsx
@@ -12,16 +12,11 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <main className=" flex flex-col items-center justify-center gap-8 mt-4 sm:mt-16 p-4 pb-0">
-      <h1 className="text-3xl text-blue-900 font-bold">Managing rental property is our life</h1>
+      <h1 className="text-3xl text-blue-900 font-bold">Profesional Landlord Services in New Jersey</h1>
       <div className="flex flex-col items-start justify-center gap-4 mt-4 p-4 sm:p-16 border-gray-200 border-y-2 max-w-[500px] sm:max-w-[620px] lg:max-w-[1000px] rounded-xl">
-        <h1 className="text-2xl font-bold text-blue-900">How we started?</h1>
         <p className="text-xl text-blue-950 text-left leading-loose">
-          TPG Management was born out of necessity and experience. As property investors ourselves, we started with managing our own multi-family and retail properties, learning firsthand the challenges
-          and opportunities that come with ownership. Over time, we saw a growing need among fellow investors for a management company that truly understood the intricacies of property ownership from an owner&apos;s perspective.
-
-          With this in mind, we decided to turn our hands-on expertise into a business. In doing so, we&apos;ve been able to help other property owners maximize their investments, 
-          streamline operations, and remove the stress of day-to-day management. Today, TPG Management continues to grow, serving New Jersey&apos;s rental property market with a focus on personalized, results-driven solutions for our clients.
-        </p>
+          Looking for reliable landlord services in New Jersey? TPG Management offers professional landlord services in NJ designed to protect your investment and maximize returns. From tenant screening and rent collection to maintenance and compliance, we handle it all so you don’t have to. Trust our experienced team to deliver hassle-free landlord services across New Jersey.
+   </p>
       </div>
       <RentalCTAAbout />
     </main>
