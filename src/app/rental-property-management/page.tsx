96bc3bd130fee634a842caae6c0984a784ba6e9c
Mike <23327684+pocketpixelsmc@users.noreply.github.com>
2025-07-30 17:54:01 -0400

    Update page.tsx

--- src/app/rental-property-management/page.tsx
+++ src/app/rental-property-management/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <h1 className="text-3xl text-blue-900 font-bold">Property Management for Rentals in New Jersey</h1>
       <div className="flex flex-col items-start justify-center gap-4 mt-4 p-4 sm:p-16 border-gray-200 border-y-2 max-w-[500px] sm:max-w-[620px] lg:max-w-[1000px] rounded-xl">
         <p className="text-xl text-blue-950 text-left leading-loose">
-        hi test  </p>
+        Looking for reliable property management for rentals in New Jersey? Our team provides end-to-end solutions for rental homes, condos, and apartment buildings, covering everything from leasing and tenant screening to maintenance coordination and rent collection. We understand the challenges rental property owners face and offer services built to protect your investment and simplify operations. Whether you own a single home or a large multi-unit building, our experienced managers deliver efficient, responsive support. </p>
       </div>
       <RentalCTAAbout />
     </main>
