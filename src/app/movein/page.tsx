dce5fa0c8652941443985af585413b1de5ceb879
Mike <23327684+pocketpixelsmc@users.noreply.github.com>
2025-08-08 15:13:03 -0400

    Create page.tsx

---
+++ src/app/movein/page.tsx
@@ -0,0 +1,10 @@
+import JotformEmbed from "../components/JotformEmbed";
+import Header from "../components/ui/Header";
+
+export default function Home() {
+  return (
+    <>
+      <JotformEmbed formId="252195487615163" />
+    </>
+  );
+}
